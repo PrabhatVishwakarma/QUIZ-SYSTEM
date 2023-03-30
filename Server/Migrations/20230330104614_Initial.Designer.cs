3c240e32c946a994c391c7cbbd813bc602b5861d
Prabhat Vishwakarma <prabhat_vishwakarma@persistent.com>
2023-03-30 16:56:36 +0530

    Updated

--- Server/Migrations/20230330104614_Initial.Designer.cs
+++ Server/Migrations/20230330104614_Initial.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Tool.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20230330083030_initial")]
-    partial class initial
+    [Migration("20230330104614_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
