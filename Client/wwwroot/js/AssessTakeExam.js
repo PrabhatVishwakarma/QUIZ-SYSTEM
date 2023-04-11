ff07b9291546552d4c1964128edf3120a29166d5
Prachi Kutemate <prachi_kutemate@persistent.com>
2023-04-11 09:25:54 +0530

    Limit to quizcategory

--- Client/wwwroot/js/AssessTakeExam.js
+++ Client/wwwroot/js/AssessTakeExam.js
@@ -1,10 +1,59 @@
 ﻿export function helloWorld() {
-   // alert('Warning for last 5 minutes!');
+  // alert('Warning for last 5 minutes!');
     var modalelement = document.getElementById("pqr")
-    modalelement.onclick
-    console.log(modalelement)
+   modalelement.onclick
+   console.log(modalelement)
 
 }
+
+
+
+
+//File: wwwroot / js / warning.js
+//export async function helloworld() {
+//    const modal = document.getElementById("pqr");
+//    modal.classList.add("show");
+
+//    await Promise.race([
+//        new Promise(resolve => modal.querySelector(".close").addEventListener("click", () => {
+//            modal.classList.remove("show");
+//            resolve("close");
+//        })),
+//        new Promise(resolve => modal.querySelector(".modal-footer1 .btn").addEventListener("click", () => {
+//            modal.classList.remove("show");
+//            resolve("continue");
+//        }))
+//    ]);
+
+//    return "continue";
+//}
+
+
+
+//export function showWarningMessage() {
+//    return new Promise(resolve => {
+//        const modal = document.getElementById("pqr");
+//        modal.classList.add("show");
+
+//        const closeButton = modal.querySelector(".close");
+//        const continueButton = modal.querySelector(".modal-footer1 .btn");
+
+//        closeButton.addEventListener("click", onClose);
+//        continueButton.addEventListener("click", onContinue);
+
+//        function onClose() {
+//            modal.classList.remove("show");
+//            resolve("continue");
+//        }
+
+//        function onContinue() {
+//            modal.classList.remove("show");
+//            resolve("continue");
+//        }
+//    });
+//}
+
+
 
 
 //export function helloWorld() {
